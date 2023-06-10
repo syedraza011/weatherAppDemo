ba1b569a760cc1793da8aa2722805fc891cdc4ae
Syed Raza <syed.raza011@gmail.com>
2023-06-10 12:14:18 -0400

    Initial Commit

---
+++ weatherAppDemo/weatherAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppDemoApp.swift
+//  weatherAppDemo
+//
+//  Created by Syed Raza on 6/10/23.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
